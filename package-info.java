2d35fd013b9657e6cd58a2c66717808dbfc7e9c8
Remy Skelton <remy.skelton@stu.ocsb.ca>
2025-04-01 03:22:20 +0000

    Done linting and code

---
+++ package-info.java
@@ -0,0 +1,11 @@
+/**
+* This is the package-info.java file
+*
+* This program prompts the user to select a file (1, 2, or 3) to process.
+* It then reads integer values from the chosen file and
+* displays the mean and median of those integers.
+*
+* @author Remy Skelton
+* @version 1.0
+* @since 2025-03-07
+*/
